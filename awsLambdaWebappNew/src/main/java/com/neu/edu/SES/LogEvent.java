4aeaabea1b37b7200c406c1ef96560a14a717934
Ruchi Niwalkar <niwalkar.r@husky.neu.edu>
2020-04-01 21:47:59 -0400

    adding config and lambda 8

--- awsLambdaWebappNew/src/main/java/com/neu/edu/SES/LogEvent.java
+++ awsLambdaWebappNew/src/main/java/com/neu/edu/SES/LogEvent.java
@@ -26,103 +26,102 @@ import java.util.Calendar;
 public class LogEvent implements RequestHandler<SNSEvent, Object> 
 {
 
-	static DynamoDB dynamoDb;
-	private String tableName = "csye6225";
-	private Regions region = Regions.US_EAST_1;
-	public String from = "";
-	static String mailSubject = "BillsDue";
-	static String htmlBody;
-	static String textBody;
-	static String token;
-	static String username;
-	 
+	private DynamoDB dynamoDB;
+	private final String TABLE_NAME = "csye6225";
+	private Regions REGION = Regions.US_EAST_1;
+	static final String DOMAIN = System.getenv("Domain");
+	static final String SUBJECT = "Bills Due";
+	private String textPart;
+	private String htmlPart;
+	private String from="";
+	private String username;
 	
 	
 	@Override
 	public Object handleRequest(SNSEvent request, Context context)
 	{
+		
 		String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
-		System.out.println("HELLO WORLD");
-	    String domain = System.getenv("DOMAIN_NAME");
-	    context.getLogger().log("Domain is :"+domain);
-	    from = "no-reply@test." + domain;
-	    context.getLogger().log("FROM EMAIL is :"+from);
-	    context.getLogger().log("Invocation started: " + timeStamp);
-        long now = Calendar.getInstance().getTimeInMillis()/1000;
-        long ttl = 30*60; // ttl set to 15 min
+        context.getLogger().log("domain"+DOMAIN);
+        from="niwalkar.r@husky.neu.edu";
+        //from = "noreply@test." + DOMAIN;
+
+        //Creating ttl
+        context.getLogger().log("Invocation started: " + timeStamp);
+        long now = Calendar.getInstance().getTimeInMillis()/1000; // unix time
+        long ttl = 30 * 60; // ttl set to 15 min
         long totalttl = ttl + now ;
+
+        //Function Excecution for sending the email
         
-        token = UUID.randomUUID().toString();
+//        try {
+//            JSONObject body = new JSONObject(input.getRecords().get(0).getSNS().getMessage());
+//            username=body.getString("emailAddress");
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+        username=request.getRecords().get(0).getSNS().getMessage();
+        context.getLogger().log("username: "+username);
+        context.getLogger().log("Invocation completed: " + timeStamp);
         
-        //Mail Sending
         try {
-        	 context.getLogger().log("Message rec "+request.getRecords().get(0).getSNS().getMessage());
-             JSONObject body = new JSONObject(request.getRecords().get(0).getSNS().getMessage());
-            username = body.getString("username");
-        } 
-        catch (JSONException e) 
-        {
-            e.printStackTrace();
-        }
-        context.getLogger().log("Username is: "+username);
-        try 
-        {
-        	AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion(region).build();
-            dynamoDb = new DynamoDB(client);
+            initDynamoDbClient();
             long ttlDbValue = 0;
-            Item item = LogEvent.dynamoDb.getTable(tableName).getItem("id", username);
-            if (item != null) 
-            {
+            Item item = this.dynamoDB.getTable(TABLE_NAME).getItem("id", username);
+            if (item != null) {
                 context.getLogger().log("Checking for timestamp");
                 ttlDbValue = item.getLong("ttl");
             }
-            if (item == null || (ttlDbValue < now && ttlDbValue != 0)) 
-            {
-            	  context.getLogger().log("Checking for valid ttl");
-                  context.getLogger().log("ttl expired, creating new token and sending email");
-                  this.dynamoDb.getTable(tableName).putItem(new PutItemSpec().withItem(new Item()
-                                  .withString("id", username)
-                                  .withString("token", token)
-                                  .withLong("ttl", totalttl)));
-                  
-                  
-                  
-                  textBody = "https://" + domain +  "/v1/bills/due/x";
-                  context.getLogger().log("Text " + textBody);
-                  htmlBody = "<h2>Email sent from Amazon SES</h2>"
-                          + "<p>Bills Due can be view in the link below. " +
-                          "Link: "+ textBody + "</p>";
-                  context.getLogger().log("This is HTML body: " + htmlBody);
-                  
-                  //Sending email using Amazon SES client
-                  AmazonSimpleEmailService clients = AmazonSimpleEmailServiceClientBuilder.standard().withRegion(region).build();
-                  
-                  SendEmailRequest emailRequest = new SendEmailRequest()
-                          .withDestination( new Destination().withToAddresses(username))
-                          .withMessage(new Message()
-                                  .withBody(new Body()
-                                          .withHtml(new Content()
-                                                  .withCharset("UTF-8").withData(htmlBody))
-                                          .withText(new Content()
-                                                  .withCharset("UTF-8").withData(textBody)))
-                                  .withSubject(new Content()
-                                          .withCharset("UTF-8").withData(mailSubject)))
-                          .withSource(from);
-                  clients.sendEmail(emailRequest);
-                  context.getLogger().log("Email sent successfully to email id: " +username);
-            }
-            else 
-            {
+
+            if (item == null || (ttlDbValue < now && ttlDbValue != 0)) {
+                context.getLogger().log("Checking for valid ttl");
+                context.getLogger().log("ttl expired, creating new token and sending email");
+                this.dynamoDB.getTable(TABLE_NAME)
+                        .putItem(
+                                new PutItemSpec().withItem(new Item()
+                                        .withString("id", username)
+                                        .withLong("ttl", totalttl)));
+
+                textPart = "Test Email Body";
+                context.getLogger().log("Text " + textPart);
+                htmlPart = "<h2>Email sent from Amazon SES</h2>" + "<p>Username is :" + username + "</p>";
+                context.getLogger().log("This is HTML body: " + htmlPart);
+
+
+                //Sending email using Amazon SES client
+                AmazonSimpleEmailService clients = AmazonSimpleEmailServiceClientBuilder.standard()
+                        .withRegion(REGION).build();
+                SendEmailRequest emailRequest = new SendEmailRequest()
+                        .withDestination(
+                                new Destination().withToAddresses(username))
+                        .withMessage(new Message()
+                                .withBody(new Body()
+                                        .withHtml(new Content()
+                                                .withCharset("UTF-8").withData(htmlPart))
+                                        .withText(new Content()
+                                                .withCharset("UTF-8").withData(textPart)))
+                                .withSubject(new Content()
+                                        .withCharset("UTF-8").withData(SUBJECT)))
+                        .withSource(from);
+                clients.sendEmail(emailRequest);
+                context.getLogger().log("Email sent successfully to email id: " +username);
+
+            } else {
                 context.getLogger().log("ttl is not expired. New request is not processed for the user: " +username);
             }
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             context.getLogger().log("Email was not sent. Error message: " + ex.getMessage());
         }
         return null;
-    }
-
+		
+	}
 	
-
+	private void initDynamoDbClient() 
+	{
+		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(REGION)
+                .build();
+		dynamoDB = new DynamoDB(client);
+	}
 }
+
